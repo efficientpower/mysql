1d2432df89d90e28741361cc7543c5810b79f4d0
wangjihui <wangjihui@debian>
2017-09-02 22:15:06 +0800

    mod:user update

--- src/main/java/org/wjh/domain/User.java
+++ src/main/java/org/wjh/domain/User.java
@@ -7,9 +7,18 @@ public class User implements Serializable {
 
     private static final long serialVersionUID = -6014182937666400888L;
 
+    public static final Integer GENDER_MALE = 1;
+    public static final Integer GENDER_FEMAL = 0;
+
     private Integer id;
     private String userId;
+    private String password;
+    private String salt;
     private String name;
+    private Integer gender;
+    private Date birthday;
+    private String photo;
+    private String signature;
     private Date dbCreateTime;
     private Date dbUpdateTime;
 
@@ -29,12 +38,60 @@ public class User implements Serializable {
         this.userId = userId;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
     }
 
     public Date getDbCreateTime() {
--- src/main/java/org/wjh/web/UserController.java
+++ src/main/java/org/wjh/web/UserController.java
@@ -1,8 +1,9 @@
 package org.wjh.web;
 
-import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +24,15 @@ public class UserController {
     public AjaxResult<Object> getUser(String userId) {
         AjaxResult<Object> res = new AjaxResult<Object>();
         try{
-            Integer[] ids = {4,3,2,1};
-            List<User> userList = userService.getByIds(Arrays.asList(ids));
+            List<User> userList = userService.list(" id ", 0, 3000);
+            for(User u : userList){
+                String passwd = u.getId() +"";
+                String salt = DigestUtils.md5Hex(u.getSalt());
+                u.setPassword(DigestUtils.md5Hex(passwd + salt));
+                u.setName("hello-" + u.getId());
+                u.setDbUpdateTime(new Date());
+            }
+            userService.batchUpdate(userList);
             res.setData(userList);
         }catch(Exception e){
             res.setCode(MobileCode.FAIL.getCode());
