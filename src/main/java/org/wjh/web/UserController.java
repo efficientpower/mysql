79d3a165433ba30492bf23832c0cdc55193ba0de
wangjihui <wangjihui@debian>
2017-09-17 12:45:46 +0800

    mod:add log4j

--- src/main/java/org/wjh/web/UserController.java
+++ src/main/java/org/wjh/web/UserController.java
@@ -1,9 +1,7 @@
 package org.wjh.web;
 
-import java.util.Date;
 import java.util.List;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,14 +23,7 @@ public class UserController {
         AjaxResult<Object> res = new AjaxResult<Object>();
         try{
             List<User> userList = userService.list(" id ", 0, 3000);
-            for(User u : userList){
-                String passwd = u.getId() +"";
-                String salt = DigestUtils.md5Hex(u.getSalt());
-                u.setPassword(DigestUtils.md5Hex(passwd + salt));
-                u.setName("hello-" + u.getId());
-                u.setDbUpdateTime(new Date());
-            }
-            userService.batchUpdate(userList);
+            userService.updateInTrans(userList.get(0).getUserId());
             res.setData(userList);
         }catch(Exception e){
             res.setCode(MobileCode.FAIL.getCode());
